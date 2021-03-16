3b746d7b225b00a26dc92ad7ef7f3ba05d5a12c1
Hatice <haticekaraty@gmail.com>
2021-03-15 20:33:04 -0400

    commit tree 41a42762b8ee78cb503371d95d99d68362e016a6

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,29 @@
 class Pokemon
+    attr_accessor :name, :type, :id, :db
+    def initialize(id:,name:,type:,db:)
+        self.id = id
+        self.name = name
+        self.type = type
+        self.db = db
+    end
+
+    def self.save(name,type,db)
+        sql = <<-SQL
+        INSERT INTO pokemon(name,type)
+        VALUES (?, ?)
+        SQL
+        db.execute(sql,name,type)
+    end
+
+    def self.find(id,db)
+        sql = <<-SQL
+        SELECT * 
+        FROM pokemon
+        WHERE id = ?
+        SQL
+        row = db.execute(sql,id).first
+        name = row[1]
+        type = row[2]
+        new_pokemon = Pokemon.new(id: id, name:name, type:type, db: db)
+    end
 end
